35d4ec6cdad99e86661bb0fe9eae6f97d78a873d
cicerotcv <elm.tiago@gmail.com>
2023-06-08 19:43:33 -0300

    refactor: add null as possible value for node type

--- src/node/interpreter-node.ts
+++ src/node/interpreter-node.ts
@@ -3,7 +3,7 @@ export interface Evaluable<T> {
 }
 
 export abstract class InterpreterNode<
-  ValueType extends string | number,
+  ValueType extends string | number | null,
   ChildrenType extends Array<any>
 > {
   value: ValueType;
