eff7207b156fd3dcd9c48bcd121be86bf0db1dc7
cicerotcv <elm.tiago@gmail.com>
2023-06-07 22:11:42 -0300

    test: refactor parser tests

--- src/parser/parser.test.ts
+++ src/parser/parser.test.ts
@@ -1,51 +1,35 @@
 import { Parser } from ".";
 
+const createParserTest = (expression: string, result: number) =>
+  test(`should evaluate '${expression}' to '${result}'`, () => {
+    expect(Parser.run(expression)).toEqual(result);
+  });
+
 describe("Parser", () => {
   describe("Expression", () => {
-    // simple binary operations
-    Object.entries({
-      "1 + 1": 2,
-      "1 - 1": 0,
-      "1 + 2 + 3": 6,
-      "1 + 2 - 3": 0,
-    }).forEach(([expression, result]) => {
-      test(`should parse '${expression}'`, () => {
-        expect(Parser.run(expression)).toEqual(result);
-      });
-    });
+    createParserTest("1 + 1", 2);
+    createParserTest("1 - 1", 0);
+    createParserTest("1 + 2 + 3", 6);
+    createParserTest("1 + 2 - 3", 0);
   });
 
   describe("Term", () => {
-    // simple binary operations
-    Object.entries({
-      "2 * 3": 6,
-      "6 / 3": 2,
-      "2 * 3 * 4": 24,
-      "24 / 3 / 2": 4,
-    }).forEach(([expression, result]) => {
-      test(`should parse '${expression}'`, () => {
-        expect(Parser.run(expression)).toEqual(result);
-      });
-    });
+    createParserTest("2 * 3", 6);
+    createParserTest("6 / 3", 2);
+    createParserTest("2 * 3 * 4", 24);
+    createParserTest("24 / 3 / 2", 4);
   });
 
   describe("Factor", () => {
-    // simple binary operations
-    Object.entries({
-      "(1 + 2 + 3)": 6,
-      "(1 + 2 - 3)": 0,
-      "1 + 2 * 3": 7,
-      "(1 + 2) * 3": 9,
-      "2 * 3 + 1": 7,
-      "2 * (3 + 1)": 8,
-      "3 + 6 / 3": 5,
-      "(3 + 6) / 3": 3,
-      "6 * 4 + 3": 27,
-      "6 * (4 + 3)": 42,
-    }).forEach(([expression, result]) => {
-      test(`should parse '${expression}'`, () => {
-        expect(Parser.run(expression)).toEqual(result);
-      });
-    });
+    createParserTest("(1 + 2 + 3)", 6);
+    createParserTest("(1 + 2 - 3)", 0);
+    createParserTest("1 + 2 * 3", 7);
+    createParserTest("(1 + 2) * 3", 9);
+    createParserTest("2 * 3 + 1", 7);
+    createParserTest("2 * (3 + 1)", 8);
+    createParserTest("3 + 6 / 3", 5);
+    createParserTest("(3 + 6) / 3", 3);
+    createParserTest("6 * 4 + 3", 27);
+    createParserTest("6 * (4 + 3)", 42);
   });
 });
